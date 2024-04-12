4f47f4be2ec794722e7355887b0bccdb4d5c7a4a
kurrx <kurr.eax@gmail.com>
2024-04-12 10:40:45 +0500

    solution: 1026 Maximum Difference Between Node and Ancestor

---
+++ solutions/1026-maximum-difference-between-node-and-ancestor.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/maximum-difference-between-node-and-ancestor
+ *
+ * TC: O(n)
+ * SC: O(n)
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function maxAncestorDiff(root) {
+  return dfs(root, root.val, root.val)
+}
+
+/**
+ * @param {TreeNode} root
+ */
+function dfs(root, min, max) {
+  if (!root) return max - min
+  min = Math.min(min, root.val)
+  max = Math.max(max, root.val)
+  const left = dfs(root.left, min, max),
+    right = dfs(root.right, min, max)
+  return Math.max(left, right)
+}
