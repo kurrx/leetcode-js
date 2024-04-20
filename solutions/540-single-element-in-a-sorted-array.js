c5f82b8bdf0433b92bf43586d36c626797e877eb
kurrx <kurr.eax@gmail.com>
2024-04-20 13:32:22 +0500

    solution: 540 Single Element in a Sorted Array

---
+++ solutions/540-single-element-in-a-sorted-array.js
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/single-element-in-a-sorted-array
+ *
+ * TC: O(log(n))
+ * SC: O(1)
+ *
+ * @param {number[]} numbers
+ * @return {number}
+ */
+function singleNonDuplicate(arr) {
+  const n = arr.length
+  if (n % 2 === 0) return -1
+
+  let left = 0,
+    right = n - 1
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2),
+      leftPair = arr[mid] === arr[mid - 1],
+      rightPair = arr[mid] === arr[mid + 1]
+    if (!leftPair && !rightPair) return arr[mid]
+    if ((mid - left + (leftPair ? -1 : 0)) % 2 === 1) {
+      if (leftPair) right = mid - 2
+      else right = mid - 1
+    } else {
+      if (rightPair) left = mid + 2
+      else left = mid + 1
+    }
+  }
+  return arr[left]
+}
