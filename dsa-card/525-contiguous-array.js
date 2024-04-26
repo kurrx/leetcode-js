74c1caaf6b1241e957a5b1fe1fe322e52f156522
kurrx <kurr.eax@gmail.com>
2024-04-26 16:15:07 +0500

    re-solve: 525

--- dsa-card/525-contiguous-array.js
+++ dsa-card/525-contiguous-array.js
@@ -7,20 +7,18 @@
  * @param {number[]} numbers
  * @return {number}
  */
-
-function findMaxLength(numbers) {
-  const n = numbers.length
-  const counter = new Map()
-  counter.set(0, -1)
-  let maxLength = 0,
-    count = 0
+function findMaxLength(nums) {
+  const n = nums.length
+  const counter = new Map([[0, -1]])
+  let count = 0,
+    max = 0
   for (let i = 0; i < n; i++) {
-    count += numbers[i] || -1
+    count += nums[i] || -1
     if (counter.has(count)) {
-      maxLength = Math.max(maxLength, i - counter.get(count))
+      max = Math.max(max, i - counter.get(count))
     } else {
       counter.set(count, i)
     }
   }
-  return maxLength
+  return max
 }
