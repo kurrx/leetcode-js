0448ae423181567e53d60dba18d6eb434fa7093e
kurrx <kurr.eax@gmail.com>
2024-04-26 19:22:08 +0500

    re-solve: 2432

--- dsa-card/2434-using-a-robot-to-print-the-lexicographically-smallest-string.js
+++ dsa-card/2434-using-a-robot-to-print-the-lexicographically-smallest-string.js
@@ -2,33 +2,39 @@
  * https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string
  *
  * TC: O(n)
- * SC: O(n)
+ * SC: O(1)
  *
  * @param {string} str
  * @return {string}
  */
-function robotWithString(str) {
-  const n = str.length
-  const maxRight = new Array(n)
-  let min = '{',
-    minIndex = -1
-  for (let i = n - 1; i >= 0; i--) {
-    if (str[i] <= min) {
-      min = str[i]
-      minIndex = i
-    }
-    maxRight[i] = minIndex
-  }
-  const paper = [],
-    robot = []
+function robotWithString(s) {
+  const n = s.length,
+    t = [],
+    counter = new Array(26)
+  let idx,
+    min = 25,
+    p = '',
+    j = 0
   for (let i = 0; i < n; i++) {
-    while (robot.length && str[maxRight[i]] >= robot[robot.length - 1]) {
-      paper.push(robot.pop())
+    idx = s.charCodeAt(i) - A_CODE
+    min = Math.min(min, idx)
+    counter[idx] = (counter[idx] ?? 0) + 1
+  }
+  while (p.length < s.length) {
+    while (j < n && t.at(-1) !== min) {
+      idx = s.charCodeAt(j) - A_CODE
+      t.push(idx)
+      counter[idx]--
+      j++
     }
-    robot.push(str[i])
+    p += String.fromCharCode(t.pop() + A_CODE)
+    for (let i = min; i < 26; i++) {
+      if (counter[i]) {
+        min = i
+        break
+      }
+    }
+    if (t.length) min = Math.min(min, t.at(-1))
   }
-  while (robot.length) {
-    paper.push(robot.pop())
-  }
-  return paper.join('')
+  return p
 }
