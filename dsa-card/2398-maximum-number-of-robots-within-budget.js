8838bf95225e2a5b09d3e3700b5aa71115ded66a
kurrx <kurr.eax@gmail.com>
2024-04-27 17:51:19 +0500

    re-solve: 2398

--- dsa-card/2398-maximum-number-of-robots-within-budget.js
+++ dsa-card/2398-maximum-number-of-robots-within-budget.js
@@ -9,23 +9,21 @@
  * @param {number} budget
  * @return {number}
  */
-function maximumRobots(times, costs, budget) {
-  const n = times.length
-  const deque = []
-  let left = 0,
-    sum = 0
-  for (let right = 0; right < n; right++) {
-    sum += costs[right]
-    while (deque.length && times[deque.at(-1)] <= times[right]) {
-      deque.pop()
+function maximumRobots(chargeTimes, runningCosts, budget) {
+  const n = chargeTimes.length,
+    stack = []
+  let sum = 0,
+    j = 0
+  for (let i = 0; i < n; i++) {
+    sum += runningCosts[i]
+    while (stack.length && chargeTimes[stack.at(-1)] <= chargeTimes[i]) {
+      stack.pop()
     }
-    deque.push(right)
-    if (times[deque[0]] + (right - left + 1) * sum > budget) {
-      if (deque[0] === left) {
-        deque.shift()
-      }
-      sum -= costs[left++]
+    stack.push(i)
+    if (chargeTimes[stack[0]] + (i - j + 1) * sum > budget) {
+      if (stack[0] === j) stack.shift()
+      sum -= runningCosts[j++]
     }
   }
-  return n - left
+  return n - j
 }
