9d7a13ab7736009912a61606b3d7752086a767ac
kurrx <kurr.eax@gmail.com>
2024-04-27 16:23:14 +0500

    re-solve: 907

--- dsa-card/907-sum-of-subarray-minimums.js
+++ dsa-card/907-sum-of-subarray-minimums.js
@@ -10,14 +10,16 @@
 function sumSubarrayMins(arr) {
   const n = arr.length,
     stack = []
-  let sum = 0
+  let sum = 0,
+    popped,
+    min
   for (let i = 0; i <= n; i++) {
-    while (stack.length && (i === n || arr[stack.at(-1)] >= arr[i])) {
-      const mid = stack.pop()
-      sum += arr[mid] * (i - mid) * (mid - (stack.at(-1) ?? -1))
-      sum %= 1_000_000_007
+    while (stack.length && (i === n || arr[stack.at(-1)] > arr[i])) {
+      popped = stack.pop()
+      min = arr[popped] * (i - popped) * (popped - (stack.at(-1) ?? -1))
+      sum = (sum + min) % 1_000_000_007
     }
-    stack.push(i)
+    if (i < n) stack.push(i)
   }
   return sum
 }
