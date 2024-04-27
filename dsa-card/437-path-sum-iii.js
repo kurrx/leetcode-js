a1fb91df89ef3d4b757865ba3bdda33c8493df15
kurrx <kurr.eax@gmail.com>
2024-04-27 23:18:05 +0500

    re-solve: 437

--- dsa-card/437-path-sum-iii.js
+++ dsa-card/437-path-sum-iii.js
@@ -9,25 +9,17 @@
  * @return {number}
  */
 function pathSum(root, targetSum) {
-  return sum(root, targetSum, 0, new Map())
-}
-
-/**
- * @param {TreeNode} root
- * @param {number} targetSum
- * @param {number} currSum
- * @param {Map<number, number>} map
- */
-function sum(root, targetSum, currSum, map) {
-  if (!root) return 0
-
-  let answer = 0
-  currSum += root.val
-  if (currSum === targetSum) answer++
-  answer += map.get(currSum - targetSum) ?? 0
-  map.set(currSum, (map.get(currSum) ?? 0) + 1)
-  if (root.left) answer += sum(root.left, targetSum, currSum, map)
-  if (root.right) answer += sum(root.right, targetSum, currSum, map)
-  map.set(currSum, (map.get(currSum) ?? 1) - 1)
-  return answer
+  function traverse(root, sum) {
+    if (!root) return 0
+    let answer = Number(sum === targetSum)
+    sum += root.val
+    answer += map.get(sum - targetSum) ?? 0
+    map.set(sum, (map.get(sum) ?? 0) + 1)
+    if (root.left) answer += traverse(root.left, sum)
+    if (root.right) answer += traverse(root.right, sum)
+    map.set(sum, map.get(sum) - 1)
+    return answer
+  }
+  const map = new Map()
+  return traverse(root, 0)
 }
