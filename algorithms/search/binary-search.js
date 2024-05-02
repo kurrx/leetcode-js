b35491acf5d18403fa01e2f3f02475d25b3502c0
kurrx <kurr.eax@gmail.com>
2024-05-02 11:54:09 +0500

    algo(search): updated binary search with more use cases

--- algorithms/search/binary-search.js
+++ algorithms/search/binary-search.js
@@ -21,3 +21,84 @@ function binarySearch(arr, target) {
   }
   return -1
 }
+
+/**
+ * Insertion point of target into sorted arr with no duplicates allowed
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ *
+ * @param {number[]} arr
+ * @param {number} target
+ * @returns {number}
+ */
+function binarySearchNoDuplicates(arr, target) {
+  let left = 0,
+    right = arr.length - 1,
+    mid
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2)
+    if (arr[mid] > target) {
+      right = mid - 1
+    } else if (arr[mid] < target) {
+      left = mid + 1
+    } else {
+      // do something
+      return mid
+    }
+  }
+  // left is the insertion point
+  return left
+}
+
+/**
+ * Left-most insertion point of target into sorted arr with duplicates allowed
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ *
+ * @param {number[]} arr
+ * @param {number} target
+ * @returns {number}
+ */
+function binarySearchDuplicatesLeft(arr, target) {
+  let left = 0,
+    right = arr.length,
+    mid
+  while (left < right) {
+    mid = Math.floor((left + right) / 2)
+    if (arr[mid] >= target) {
+      right = mid
+    } else {
+      left = mid + 1
+    }
+  }
+  // left is the insertion point
+  return left
+}
+
+/**
+ * Left-most insertion point of target into sorted arr with duplicates allowed
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ *
+ * @param {number[]} arr
+ * @param {number} target
+ * @returns {number}
+ */
+function binarySearchDuplicatesRight(arr, target) {
+  let left = 0,
+    right = arr.length,
+    mid
+  while (left < right) {
+    mid = Math.floor((left + right) / 2)
+    if (arr[mid] > target) {
+      right = mid
+    } else {
+      left = mid + 1
+    }
+  }
+  // right is the insertion point
+  return left
+}
