9d0d0c79cabb9410fab48f2d53a722f77a0cb2fb
kurrx <kurr.eax@gmail.com>
2024-05-15 14:23:10 +0500

    solution: 287

---
+++ neetcode/287-find-the-duplicate-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findDuplicate(nums) {
+  let slow = nums[0],
+    fast = nums[0]
+  do {
+    slow = nums[slow]
+    fast = nums[nums[fast]]
+  } while (slow != fast)
+  slow = nums[0]
+  while (slow != fast) {
+    slow = nums[slow]
+    fast = nums[fast]
+  }
+  return fast
+}
