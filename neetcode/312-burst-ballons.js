d8749815778f76a35fc85c457afa2c12439ee455
kurrx <kurr.eax@gmail.com>
2024-05-17 11:10:41 +0500

    improved: 312

--- neetcode/312-burst-ballons.js
+++ neetcode/312-burst-ballons.js
@@ -3,27 +3,41 @@
  * @return {number}
  */
 function maxCoins(nums) {
-  function dp(left, right) {
-    if (left > right) return 0
-    if (memo.has(left)) {
-      if (memo.get(left).has(right)) return memo.get(left).get(right)
-    } else {
-      memo.set(left, new Map())
+  // Special Case: only one number, only one burst
+  if (nums.length === 1) return nums[0]
+  // Special Case: all numbers same
+  let allSame = true
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] !== nums[i]) {
+      allSame = false
+      break
     }
-    let max = 0
-    for (let i = left; i <= right; i++) {
-      max = Math.max(
-        max,
-        dp(left, i - 1) +
-          nums[left - 1] * nums[i] * nums[right + 1] +
-          dp(i + 1, right),
-      )
-    }
-    memo.get(left).set(right, max)
-    return max
   }
-  const memo = new Map()
+  if (allSame) return nums[0] ** 3 * (nums.length - 2) + nums[0] ** 2 + nums[0]
+
+  // Edge Case
   nums.unshift(1)
   nums.push(1)
-  return dp(1, nums.length - 2)
+  // Table
+  const n = nums.length,
+    dp = []
+  for (let i = 0; i < n; i++) dp.push(new Array(n).fill(0))
+  // DP
+  for (let left = n - 2; left >= 1; left--) {
+    for (let right = left; right < n - 1; right++) {
+      for (let i = left; i <= right; i++) {
+        dp[left][right] = Math.max(
+          dp[left][right],
+          dp[left][i - 1] +
+            dp[i + 1][right] +
+            nums[left - 1] * nums[i] * nums[right + 1],
+        )
+      }
+    }
+  }
+
+  // Remove Edge Cases
+  nums.shift()
+  nums.pop()
+  return dp[1][n - 2]
 }
