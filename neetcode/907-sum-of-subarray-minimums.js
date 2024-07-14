d64efdf6a6acaa026d7b6d469f7ed24c0ea4a232
kurrx <kurr.eax@gmail.com>
2024-07-14 11:26:32 +0500

    solution: 907

---
+++ neetcode/907-sum-of-subarray-minimums.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+function sumSubarrayMins(arr) {
+  const n = arr.length,
+    stack = []
+  let sum = 0,
+    popped,
+    min
+  for (let i = 0; i <= n; i++) {
+    while (stack.length && (i === n || arr[stack.at(-1)] > arr[i])) {
+      popped = stack.pop()
+      min = arr[popped] * (i - popped) * (popped - (stack.at(-1) ?? -1))
+      sum = (sum + min) % 1_000_000_007
+    }
+    if (i < n) stack.push(i)
+  }
+  return sum
+}
